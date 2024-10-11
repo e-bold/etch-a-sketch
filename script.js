8b545f2f7075b2e06d842c8dfb0cd8432cec29d6
E. Enkhbold <enkhmunkh@att.net>
2024-10-11 14:58:03 -0500

    Updated script.js Added button for the prompt

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const container = document.createElement('div');
-const body = document.querySelector('body');
-
-container.className = "box";
-
-
-body.append(container)
+const body = document.querySelector('body'); 
+const container = document.querySelector(".container")
+const button = document.querySelector('.pixcount')
+let sides = 16;
 
 
-for (let i = 0; i < 256; i++) {
-    const pixel = document.createElement('div')
+
+
+
+button.addEventListener('click', () => {
+    let input = prompt("How many sides do you want?")
+    if (input !== isNaN || input > 40) {
+        alert("please enter a number between 1 - 40")
+    } else {
+        input = parseInt(input);
+        sides = input;
+    }
+})
+
+
+
+
+
+
+
+
+
+
+for (let i = 0; i < sides*sides; i++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
     container.append(pixel);
-    pixel.classList.add('pixel');  
-    pixel.addEventListener('hover', )
+    
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = "black";
+    })  
+    pixel.addEventListener('mouseout', () => {
+        pixel.style.backgroundColor = "red";
+    })  
 }
+
