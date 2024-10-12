1e2bfc677261a33848c7800d02544161c0a5629b
e-bold <enkhmunkh@att.net>
2024-10-12 18:04:16 -0500

    Completed task Added clear btn and logic when cleared the grid size remains the same. Modified the CSS styling. Assigned dark background color and pastel colors for the pixels.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const body = document.querySelector('body'); 
 const container = document.querySelector(".container")
 const button = document.querySelector('.pixcount')
+const clearBtn = document.querySelector('.clear-btn')
+
+let currentSides = 16; //default grid when page is loaded
 containerWidth=container.offsetWidth;
-
-
-
 
 
 
 button.addEventListener('click', askPrompt);
 
 
+clearBtn.addEventListener('click', () => {
+    createSides(currentSides);
+});
+
+
+
+
 function createSides (sides) {
+    currentSides = sides;
     container.innerHTML = '';
     const pixelSize = containerWidth / sides;
-    
+
     console.log("Container width: " +containerWidth)
     console.log("Sides " + sides)
     console.log("Pixel size " + pixelSize)
@@ -28,26 +36,37 @@ function createSides (sides) {
         container.append(pixel);
 
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = "black";
+            pixel.style.backgroundColor = "red";
         })  
         pixel.addEventListener('mouseout', () => {
-            pixel.style.backgroundColor = "red";
+            pixel.style.backgroundColor = generateRandomPastelColor();
+            
         })  
     }
 }
 
 function askPrompt () {
-    let input = prompt("How many sides do you want(1-100)?")
+    let input = prompt("Enter your desired grid size (1-50)?")
     
-    if (isNaN(input) || input > 100 || input < 1) {
-        alert("Please enter a number between 1-100")
+    if (isNaN(input) || input > 50 || input < 1) {
+        alert("Please enter a number between 1-100!")
     } else {
-        createSides(parseInt(input))
+        currentSides = parseInt(input);
+        createSides(currentSides);
     }
 }
 
 
-createSides(15);
+function generateRandomPastelColor() {
+    const hue = Math.random() * 360;
+    const saturation = 0.5 + (Math.random() * 0.3); // softer saturation
+    const lightness = 0.7 + (Math.random() * 0.2); // lighter tone
+    return `hsl(${hue}, ${saturation*100}%, ${lightness*100}%)`;
+}
+
+
+
+createSides(currentSides);
 
 
 
