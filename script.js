600a29e76ec81b883f743148364e214e23371a96
e-bold <enkhmunkh@att.net>
2024-10-12 16:16:18 -0500

    The grid shows correctly now Fixed issue where pixels where not fitting inside th econtainer

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const body = document.querySelector('body');
 const container = document.querySelector(".container")
 const button = document.querySelector('.pixcount')
 containerWidth=container.offsetWidth;
-let sides = 16;
-
 
 
 
@@ -13,14 +11,22 @@ let sides = 16;
 button.addEventListener('click', askPrompt);
 
 
-function createSides () {
+function createSides (sides) {
+    container.innerHTML = '';
+    const pixelSize = containerWidth / sides;
+    
+    console.log("Container width: " +containerWidth)
+    console.log("Sides " + sides)
+    console.log("Pixel size " + pixelSize)
+
     for (let i = 0; i < sides*sides; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
-        pixel.style.width = containerWidth/sides +"px";
-        pixel.style.height = containerWidth/sides + "px";
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
 
         container.append(pixel);
+
         pixel.addEventListener('mouseover', () => {
             pixel.style.backgroundColor = "black";
         })  
@@ -31,21 +37,17 @@ function createSides () {
 }
 
 function askPrompt () {
-    let input = prompt("How many sides do you want?")
+    let input = prompt("How many sides do you want(1-100)?")
     
-    if (isNaN(input) || input > 40) {
-        alert("please enter a number between 1 - 40")
-        
+    if (isNaN(input) || input > 100 || input < 1) {
+        alert("Please enter a number between 1-100")
     } else {
-        input = parseInt(input);
-        sides = input;
-        alert(sides);
+        createSides(parseInt(input))
     }
 }
 
-createSides()
 
-
+createSides(15);
 
 
 
